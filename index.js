3445dab7b189f57b7dc68c3a539e3082bdd20209
Pauteau <30441088+Pauteau@users.noreply.github.com>
2018-10-06 08:19:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("h");
+
+bot.on('ready', () => {
+    console.log("HabboChest | En ligne")
+});
+
+bot.login('NDk3ODAwMTE5Mjk5ODY2NjI1.DpnjFA.mxsobW5V1UjEtUehZHC2r1v_qa8');
